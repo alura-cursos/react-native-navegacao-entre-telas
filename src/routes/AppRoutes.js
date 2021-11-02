0a35c21cd5ed9bd2f586faea7ef00d4bcf453883
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-11-02 14:54:36 -0300

    Adding svg icons

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,6 +6,9 @@ import Home from '../telas/Home';
 import Cesta from '../telas/Cesta';
 import mock from '../mocks/cesta';
 
+import HomeSvg from '../assets/home.svg';
+import CoracaoSvg from '../assets/coracao.svg';
+
 const Tab = createBottomTabNavigator();
 
 const CestaScreen = () => <Cesta {...mock} />;
@@ -13,7 +16,23 @@ const CestaScreen = () => <Cesta {...mock} />;
 export default function AppRoutes() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let Icon;
+
+            if (route.name === 'Home') {
+              Icon = HomeSvg;
+            } else if (route.name === 'Cesta') {
+              Icon = CoracaoSvg
+            }
+
+            return <Icon color={focused ? '#2A9F85' : '#C7C7C7'} />;
+          },
+          tabBarActiveTintColor: '#2A9F85',
+          tabBarInactiveTintColor: '#C7C7C7',
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Cesta" component={CestaScreen} />
       </Tab.Navigator>
