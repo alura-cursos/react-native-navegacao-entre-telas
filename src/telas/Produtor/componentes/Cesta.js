55c5758595b4e2ec4e0219832d2e7a44df59b4db
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-12-29 14:17:39 -0300

    Aula 3.4

---
+++ src/telas/Produtor/componentes/Cesta.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/core';
+
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import Texto from '../../../componentes/Texto';
+
+export default function Cesta({ detalhes, itens, produtor }) {
+    const navigation = useNavigation();
+    const { nome, imagem, descricao, preco } = detalhes;
+
+    return <TouchableOpacity
+        style={estilos.cesta}
+        onPress={() => {}}>
+        <View style={estilos.conteudo}>
+            <Image source={imagem} style={estilos.imagem} />
+
+            <View style={estilos.textos}>
+                <Texto style={estilos.nome}>{nome}</Texto>
+                <Texto style={estilos.descricao}>{descricao}</Texto>
+                <Texto style={estilos.preco}>{preco}</Texto>
+            </View>
+        </View>
+    </TouchableOpacity>
+}
+
+const estilos = StyleSheet.create({
+    cesta: {
+        paddingHorizontal: 16,
+    },
+    conteudo: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#ECECEC',
+        paddingVertical: 16,
+    },
+    imagem: {
+        width: 62,
+        height: 62,
+        borderRadius: 6,
+    },
+    textos: {
+        flex: 1,
+        marginLeft: 8,
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 14,
+        lineHeight: 22,
+        fontWeight: 'bold',
+    },
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 14,
+        lineHeight: 22,
+    },
+    preco: {
+        color: '#2A9F85',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold',
+        marginTop: 4,
+    }
+});
--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
-import { Text } from 'react-native';
+import useTextos from '../../hooks/useTextos';
+
+import { FlatList, Text, View, Image, StyleSheet } from 'react-native';
+
+import Cesta from './componentes/Cesta';
+import Topo from '../../componentes/Topo';
+
+import topo from '../../assets/produtores/topo.png';
 
 export default function Produtor() {
     const route = useRoute();
-    console.log(route.params);
+    const { tituloProdutor, tituloCestas } = useTextos();
+    const { nome, imagem, cestas } = route.params;
 
-    return <Text>Produtor</Text>
+    const TopoLista = () => {
+        return <>
+            <Topo titulo={tituloProdutor} imagem={topo} altura={150} />
+            <View style={estilos.conteudo}>
+                <View style={estilos.logo}>
+                    <Image source={imagem} style={estilos.produtorImage} />
+                    <Text style={estilos.produtor}>{nome}</Text>
+                </View>
+
+                <Text style={estilos.cestas}>{ tituloCestas }</Text>
+            </View>
+        </>
+    }
+
+    return <FlatList
+        ListHeaderComponent={TopoLista}
+        data={cestas}
+        renderItem={({ item }) => <Cesta {...item} produtor={{ nome, imagem }}/>}
+        style={estilos.lista}
+    />
+
 }
+
+const estilos = StyleSheet.create({
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    conteudo: {
+        paddingHorizontal: 16,
+    },
+    logo: {
+        flexDirection: 'row',
+    },
+    produtorImage: {
+        width: 62,
+        height: 62,
+
+        marginTop: -23,
+
+        borderRadius: 6,
+    },
+    produtor: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginLeft: 16,
+    },
+    cestas: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginTop: 32,
+    }
+});
