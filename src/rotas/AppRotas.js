f8df0eed36ec2f260132ff9d87ba80d54386ea6e
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2022-01-06 09:25:23 -0300

    Aula 4.2

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -6,11 +6,27 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
+
 const Tab = createBottomTabNavigator();
 
 export default function AppRotas() {
     return <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            let Icon = Home;
+
+            if (route.name === 'Melhores Produtores') {
+              Icon = Coracao;
+            }
+
+            return <Icon color={color} />
+          },
+          tabBarActiveTintColor: '#2A9F85',
+          tabBarInactiveTintColor: '#C7C7C7',
+        })}>
         <Tab.Screen name='Home' component={ProdutorRotas} />
         <Tab.Screen name='Melhores Produtores' component={MelhoresProdutoresRotas} />
       </Tab.Navigator>
