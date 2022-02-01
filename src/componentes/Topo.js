4df71af8db9372dffdb385541cf65eb1650ded34
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2022-02-01 11:22:41 -0300

    Imagem voltar como svg

--- src/componentes/Topo.js
+++ src/componentes/Topo.js
@@ -6,7 +6,7 @@ import Texto from './Texto';
 
 import Gradiente from '../assets/gradiente.svg';
 import topo from '../assets/topo.png';
-import voltar from '../assets/voltar.png';
+import VoltarSVG from '../assets/voltar.svg';
 
 const largura = Dimensions.get('screen').width;
 const ALTURA_PADRAO = 270;
@@ -21,7 +21,7 @@ export default function Topo({ titulo, imagem = topo, altura = ALTURA_PADRAO }) 
     <TouchableOpacity 
       onPress={() => { navigation.goBack() }}
       style={estilos.botaoVoltar}>
-      <Image source={voltar} style={estilos.voltar} />
+      <VoltarSVG color='white' style={estilos.voltar} />
     </TouchableOpacity>
   </>
 }
